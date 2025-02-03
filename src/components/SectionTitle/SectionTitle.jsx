709d9a081bd5910918fcc87305f7e4225d8a65ae
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2025-02-03 15:54:56 +0600

    Complete Dynamic Section

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div className='md:w-3/12 text-center pb-10 mx-auto'>
+        <div className='md:w-3/12 text-center pb-10 mx-auto '>
             <p className='text-yellow-500 font-semibold text-md py-2'>{subHeading}</p>
             <h3 className='text-4xl font-semibold border-y-4 py-2'>{heading}</h3>
         </div>
