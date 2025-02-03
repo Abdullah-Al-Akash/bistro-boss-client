42069d50952dbb2ab5598d66b588c0ab2e2a5ab9
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2025-02-03 15:54:37 +0600

    Complete Dynamic Section

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -4,16 +4,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from 'swiper/modules';
-import slide1 from '../../../assets/home/slide1.jpg'
-import slide2 from '../../../assets/home/slide2.jpg'
-import slide3 from '../../../assets/home/slide3.jpg'
-import slide4 from '../../../assets/home/slide4.jpg'
-import slide5 from '../../../assets/home/slide5.jpg'
+import { Pagination } from "swiper/modules";
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
     <div className="md:px-48 px-12 py-8">
+      <div>
+        <SectionTitle
+        subHeading={"---From 11:00am to 10:00pm---"}
+        heading={"ORDER ONLINE"}
+        ></SectionTitle>
+      </div>
       <Swiper
         slidesPerView={4}
         spaceBetween={20}
@@ -24,24 +31,34 @@ const Category = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-            <img src={slide1} alt="" />
-            <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">Salads</h3>
+          <img src={slide1} alt="" />
+          <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">
+            Salads
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-            <img src={slide2} alt="" />
-            <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">Pizzas</h3>
+          <img src={slide2} alt="" />
+          <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">
+            Pizzas
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-            <img src={slide3} alt="" />
-            <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">Soups</h3>
+          <img src={slide3} alt="" />
+          <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">
+            Soups
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-            <img src={slide4} alt="" />
-            <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">Deserts</h3>
+          <img src={slide4} alt="" />
+          <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">
+            Deserts
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-            <img src={slide5} alt="" />
-            <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">Salads</h3>
+          <img src={slide5} alt="" />
+          <h3 className="uppercase md:text-3xl md:-mt-20 -mt-12 text-center font-semibold text-white">
+            Salads
+          </h3>
         </SwiperSlide>
       </Swiper>
     </div>
