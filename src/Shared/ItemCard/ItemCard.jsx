de23542288763d05b32c6dea00505ff1f41dae30
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2025-02-04 15:46:51 +0600

    Update Featured Image

--- src/Shared/ItemCard/ItemCard.jsx
+++ src/Shared/ItemCard/ItemCard.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ItemCard = ({item}) => {
     const{name,recipe,image,price} = item;
     return (
-        <div className='flex space-x-2 p-10 shadow-md rounded'>
-            <img style={{borderRadius: '0 200px 200px 200px'}} className='w-[120px]' src={image} alt="" />
+        <div className='md:flex space-x-2 p-10 shadow-md rounded'>
+            <img style={{borderRadius: '0 200px 200px 200px'}} className='w-[150px] mx-auto' src={image} alt="" />
             <div className='p-2'>
-                <h3 className='text-3xl font-semibold'>{name}-------------</h3>
+                <h3 className='text-3xl font-semibold'>{name}-----------</h3>
                 <p className='text-lg text-gray-500 pt-2'>{recipe}</p>
             </div>
             <p className='text-yellow-500 text-2xl font-semibold'>${price}</p>
