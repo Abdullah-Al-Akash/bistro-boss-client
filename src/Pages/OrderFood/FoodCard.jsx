9555f5f0bb06df8033efccd4bf3a4da072ad304f
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2025-02-08 14:57:32 +0600

    Price Position fixed in FoodCard

--- src/Pages/OrderFood/FoodCard.jsx
+++ src/Pages/OrderFood/FoodCard.jsx
@@ -7,6 +7,8 @@ const FoodCard = ({ food }) => {
       <figure>
         <img className="w-full rounded-none" src={image} alt="Shoes" />
       </figure>
+      
+      <p className="font-semibold rounded-md absolute top-5 right-5 text-white bg-black px-4 py-2">${price}</p>
       <div className="card-body">
         <h2 className="text-center md:text-2xl text-xl font-semibold">
           {name}
